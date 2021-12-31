f4b8373382a8444e804a2f2a16493a07cf3a9c1a
thanhtuan472k <tuango.472000@gmail.com>
2021-12-31 17:40:01 +0700

    first commit

---
+++ client/src/components/mainPages/utils/slider/SliderData.js
@@ -0,0 +1,12 @@
+export const SliderData = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/rpharshavarthan/image/upload/v1623607588/slider1_vlpiur.png",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/du0zjoe91/image/upload/v1640750008/truong-dai-hoc-o-da-nang-dai-hoc-bach-khoa_j12ik5.jpg",
+  },
+];
