ed619f239b9cbd7bca331e46877527878c513c9d
thanhtuan472k <tuango.472000@gmail.com>
2022-01-11 21:51:26 +0700

    fix somethings

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-require("dotenv").config();
-const express = require("express");
-const http = require("http");
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
 // const socketio = require('socket.io');
-const mongoose = require("mongoose");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const fileUpload = require("express-fileupload");
-const auth = require("./middleware/auth");
+const mongoose = require('mongoose');
+const pat = require('path');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const auth = require('./middleware/auth');
 
 //
 const app = express();
 const server = http.createServer(app);
-const io = require("socket.io")(server)
-  //{
+const io = require('socket.io')(server);
+//{
 //   cors: {
 //     origin: "http://localhost:3000",
 //   },
@@ -20,40 +21,40 @@ const io = require("socket.io")(server)
 let users = [];
 
 const addUser = (userId, socketId) => {
-  !users.some((user) => user.id === userId) && users.push({ userId, socketId });
+    !users.some((user) => user.id === userId) && users.push({ userId, socketId });
 };
 
 const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
+    users = users.filter((user) => user.socketId !== socketId);
 };
 
 const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
+    return users.find((user) => user.userId === userId);
 };
 //
-io.on("connection", (socket) => {
-  console.log("a user connected.");
-  socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
-
-  //send message
-  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    console.log(receiverId);
-    console.log(users);
-    const user = getUser(receiverId);
-    io.to(user?.socketId).emit("getMessage", {
-      senderId,
-      text,
+io.on('connection', (socket) => {
+    console.log('a user connected.');
+    socket.on('addUser', (userId) => {
+        addUser(userId, socket.id);
+        io.emit('getUsers', users);
     });
-  });
 
-  socket.on("disconnect", () => {
-    console.log("a user disconnected");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
-  });
+    //send message
+    socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+        console.log(receiverId);
+        console.log(users);
+        const user = getUser(receiverId);
+        io.to(user?.socketId).emit('getMessage', {
+            senderId,
+            text,
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+        removeUser(socket.id);
+        io.emit('getUsers', users);
+    });
 });
 
 //
@@ -61,27 +62,27 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(
-  fileUpload({
-    useTempFiles: true,
-  })
+    fileUpload({
+        useTempFiles: true,
+    })
 );
 
 //Routes
-app.use("/user", require("./routes/user.route"));
-app.use("/rooms", auth, require("./routes/chat.route"));
-app.use("/api", require("./routes/category.route"));
-app.use("/api", require("./routes/upload.route"));
-app.use("/api", require("./routes/post.route"));
-app.use("/api", require("./routes/ad.route"));
+app.use('/user', require('./routes/user.route'));
+app.use('/rooms', auth, require('./routes/chat.route'));
+app.use('/api', require('./routes/category.route'));
+app.use('/api', require('./routes/upload.route'));
+app.use('/api', require('./routes/post.route'));
+app.use('/api', require('./routes/ad.route'));
 
 //
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
-  })
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    });
 }
 // app.get("*", (req, res) => {
 //   return res.status(404).json({
@@ -95,21 +96,21 @@ const PORT = process.env.PORT || 5000;
 
 //
 mongoose
-  .connect(URI, {
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("connected to db successfully"))
-  .catch((e) => console.log(e));
+    .connect(URI, {
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('connected to db successfully'))
+    .catch((e) => console.log(e));
 
 //
-app.get("/", (req, res) => {
-  res.json({ msg: "welcome" });
+app.get('/', (req, res) => {
+    res.json({ msg: 'welcome' });
 });
 
 //
 server.listen(PORT, () => {
-  console.log("server is listening at", PORT);
+    console.log('server is listening at', PORT);
 });
