ecd578b03cdced2aec09b4ed668ef0b9a7031b82
thanhtuan472k <tuango.472000@gmail.com>
2022-01-18 23:49:35 +0700

    change style filter

--- client/src/components/mainPages/utils/filters/Filter.js
+++ client/src/components/mainPages/utils/filters/Filter.js
@@ -1,55 +1,59 @@
-import React, { useContext } from "react";
-import { GlobalState } from "../../../../globalState";
+import React, { useContext } from 'react';
+import { GlobalState } from '../../../../globalState';
 
 export default function Filter() {
-  const state = useContext(GlobalState);
-  const [categories] = state.postsAPI.categories;
-  const [category, setCategory] = state.postsAPI.category;
-  const [sort, setSort] = state.postsAPI.sort;
-  const [search, setSearch] = state.postsAPI.search;
+    const state = useContext(GlobalState);
+    const [categories] = state.postsAPI.categories;
+    const [category, setCategory] = state.postsAPI.category;
+    const [sort, setSort] = state.postsAPI.sort;
+    const [search, setSearch] = state.postsAPI.search;
 
-  const handleClick = (e) => {
-    setCategory(e.target.id);
-    console.log(e.target.id);
-  };
+    const handleClick = (e) => {
+        setCategory(e.target.id);
+        console.log(e.target.id);
+    };
 
-  
+    return (
+        <div className="filter">
+            <div className="filter-left">
+                <ul>
+                    <li className="filter-category active" id="" onClick={handleClick}>
+                        Tất cả
+                    </li>
+                    {categories.map((category) => {
+                        return (
+                            <li
+                                className="filter-category"
+                                id={'category=' + category.name}
+                                key={category._id}
+                                onClick={handleClick}
+                            >
+                                {category.name}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <div className="filter-right">
+                <div>
+                    <input
+                        className="filter-input"
+                        type="text"
+                        value={search}
+                        placeholder="Tìm kiếm món đồ"
+                        onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
+                    />
+                </div>
 
-  return (
-    <div className="filter_menu bg-dark">
-      <div className="filter-row">
-        <span id="" onClick={handleClick}>
-          All
-        </span>
-        {categories.map((category) => {
-          return (
-            <span
-              className="cat"
-              id={"category=" + category.name}
-              key={category._id}
-              onClick={handleClick}
-            >
-              {category.name}
-            </span>
-          );
-        })}
-      </div>
-      <div className="search-box">
-        <input
-          type="text"
-          value={search}
-          placeholder="Tìm kiếm món đồ"
-          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
-        />
-        <div className="sort">
-          <select value={sort} onChange={(e) => setSort(e.target.value)}>
-            <option value="sort=">Mới nhất</option>
-            <option value="sort=oldest">Cũ hơn</option>
-            <option value="sort=-price">Giá giảm dần</option>
-            <option value="sort=price">Giá tăng dần</option>
-          </select>
+                <div className="filter-lable">
+                    <select value={sort} onChange={(e) => setSort(e.target.value)}>
+                        <option value="sort=">Mới nhất</option>
+                        <option value="sort=oldest">Cũ hơn</option>
+                        <option value="sort=-price">Giá giảm dần</option>
+                        <option value="sort=price">Giá tăng dần</option>
+                    </select>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
