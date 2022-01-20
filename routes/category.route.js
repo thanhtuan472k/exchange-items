1f0a51cee26f2b2733ba7029ec91e7f561947d3d
thanhtuan472k <tuango.472000@gmail.com>
2022-01-20 19:42:10 +0700

    add admin middleware

--- routes/category.route.js
+++ routes/category.route.js
@@ -3,9 +3,7 @@ const categoryCtrl = require("../controllers/category.controller");
 const auth = require('../middleware/auth');
 const adminAuth = require('../middleware/adminAuth');
 
-router.route('/category')
-    .get(categoryCtrl.getCategory)
-    .post( categoryCtrl.createCategory)
+router.route('/category').get(categoryCtrl.getCategory).post(auth, adminAuth, categoryCtrl.createCategory);
 router.route("/category/:id")
     .delete(auth, adminAuth, categoryCtrl.deleteCategory)
     .put(auth, adminAuth, categoryCtrl.updateCategory);
